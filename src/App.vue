29846f05f4b8c376d09b63af712e4f39ab356040
NazarethCam <naza3192@gmail.com>
2022-03-19 13:29:16 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div> 
+    <h2>
+    Tarea
+    <input
+      type="text"
+      v-model="nuevaTarea"
+      placeholder="ingresa nueva tarea"
+      @keydown.enter="agregarNuevaTarea"
+    />
+    <button @click="agregarNuevaTarea">Añade</button>
+    <Lista :tareas="tareas" @delete-item="eliminarTarea"></Lista>
+    </h2>
+  </div>
+</template>
+
+<script>
+import Lista from "./components/Lista";
+export default {
+  data() {
+    return {
+      nuevaTarea: "",
+      tareas: [],
+    };
+  },
+  components: { Lista },
+  methods: {
+    agregarNuevaTarea() {
+      this.tareas.push(this.nuevaTarea);
+      this.nuevaTarea = "";
+    },
+
+    eliminarTarea(index) {
+      this.tareas.splice(index, 1);
+    },
+  },
+};
+</script>
+
+
---
+++ src/components/Lista.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in tareas" :key="index">
+        {{ item }} <button @click="deleteItem(index)">eliminar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {};
+  },
+  props: ["tareas"],
+  methods: {
+    deleteItem(index) {
+      this.$emit("delete-item", index);
+    },
+  },
+};
+</script>
+
+<style>
+
+div {
+font-family: 'Montserrat', sans-serif;
+display: flex;
+justify-content: center;
+margin-top: 5rem;
+}
+
+h2 {
+  margin: 4rem 6rem;
+
+}
+
+button {
+  margin: 0.2rem;
+  background-color: white;
+  border-radius: 10%;
+  border-color: rgb(206, 202, 202);
+  padding: 0.1rem 0.5rem;
+}
+
+input {
+border: 2px solid rgb(206, 202, 202);
+}
+
+ul {
+  margin-top: 1rem;
+}
+</style>
